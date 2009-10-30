8fb1509d3bc2b74820639cfaab2180dea1c7838a
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2009-10-30 18:48:14 +0900

    leaked committing files. fix it.

---
+++ include/bbviewer.h
@@ -0,0 +1,49 @@
+/*
+ * BitboardCA. Cellular Automata using bitboard algorithms.
+ * Copyright (C) Toshiyuki Hirooka <toshi.hirooka@gmail.com> http://wasabi.in/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+#pragma once
+
+#include "bbcommon.h"
+
+#include <iostream>
+#include <vector>
+
+
+namespace BitboardCA
+{
+	class BitboardViewer
+	{
+		// output target
+		std::ostream * m_pOstream;
+
+		public:
+			BitboardViewer(std::ostream * ostream = &std::cout);
+
+			/** view bitboard */
+			void ViewBitboard(Bitboard b);
+
+			/** view Bitboard info */
+			void ViewBitboardInfo(Bitboard * bitboards, std::size_t size_x, std::size_t size_y);
+
+			/** view one line Bitboard at position y */
+			void ViewBitboardLine(Bitboard b, std::size_t y);
+	};
+}
+
---
+++ src/bbviewer.cpp
@@ -0,0 +1,72 @@
+/*
+ * BitboardCA. Cellular Automata using bitboard algorithms.
+ * Copyright (C) Toshiyuki Hirooka <toshi.hirooka@gmail.com> http://wasabi.in/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "bbviewer.h"
+
+
+namespace BitboardCA
+{
+	BitboardViewer::BitboardViewer(std::ostream * ostream)
+	{
+		m_pOstream = ostream;
+	}
+
+	void BitboardViewer::ViewBitboardLine(Bitboard b, std::size_t y)
+	{
+		b >>= y * 8;
+		for ( std::size_t i = 0 ; i < 8 ; i++ )
+		{
+			if ( b % 2 == 1 )
+				(*m_pOstream) << "* ";
+			else
+				(*m_pOstream) << "  ";
+			b >>= 1;
+		}
+	}
+
+	void BitboardViewer::ViewBitboard(Bitboard b)
+	{
+		for ( std::size_t i = 0 ; i < 8 ; i++ )
+		{
+			ViewBitboardLine(b, i);
+			(*m_pOstream) << std::endl;
+		}
+	}
+
+	void BitboardViewer::ViewBitboardInfo(Bitboard * bitboards, std::size_t size_x, std::size_t size_y)
+	{
+		(*m_pOstream) << "-- Bitboard Info --" << std::endl;
+
+		for ( std::size_t x = 0 ; x < size_x ; x++ )
+		{
+			for ( std::size_t y = 0 ; y < size_y ; y++ )
+			{
+				ViewBitboard(bitboards[x + y * size_x]);
+				//for ( std::size_t yy = 0 ; y < 8 ; y++ )
+				//{
+				//	ViewBitboardLine(bitboards[x + y * size_x], yy);
+				//	(*m_pOstream) << " ";
+				//}
+				(*m_pOstream) << std::endl;
+			}
+			(*m_pOstream) << std::endl;
+		}
+	}
+
+}
